250c1e1d7060199cf7eb4255f7750fe04d06d40d
Domino-111 <dom.walsh@outlook.com>
2024-10-31 11:22:19 +1100

    Addition of proper gun system

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
@@ -7,19 +8,23 @@ public class Bullet : MonoBehaviour
     //public float speed = 20f;
     private float timeElapsed = 0f;
 
-    
-    void Update()
-    {
-        transform.position += transform.right * Time.deltaTime;
+    public float speed;
 
-        timeElapsed += Time.deltaTime;
-        if (timeElapsed > 5f)
-        {
-            Destroy(gameObject);
-        }
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    void Update()
+    {
+        rb.AddForce(transform.forward * speed * Time.deltaTime);
+
+        Destroy(gameObject, 3f);
+    }
+
+    private void OnCollisionEnter(Collision collision)
     {
         if (collision.transform.tag == "Asteroids")
         {
--- Assets/Scripts/ShipGun.cs
+++ Assets/Scripts/ShipGun.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class ShipGun : MonoBehaviour
 {
-    public Rigidbody laser;
+    public GameObject laser;
     public float shootSpeed = 300;
+    public Transform firepoint;
 
     void Update()
     {
@@ -17,12 +18,6 @@ public class ShipGun : MonoBehaviour
 
     void shootBullet()
     {
-        // Instantiate a new bullet at the players position and rotation
-        // later you might want to add an offset here or 
-        // use a dedicated spawn transform under the player
-        var projectile = Instantiate(laser, transform.position, transform.rotation);
-
-        //Shoot the Bullet in the forward direction of the player
-        projectile.velocity = transform.forward * shootSpeed;
+        Instantiate(laser, firepoint.position, Quaternion.identity);
     }
 }
